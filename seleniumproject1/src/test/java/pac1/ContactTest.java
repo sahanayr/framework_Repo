401a5141eb1d61e0e2a323fa8dc691509b960987
ADMIN <ADMIN@DESKTOP-EJHAJJ4.mshome.net>
2025-09-26 16:37:35 +0530

    commit

---
+++ seleniumproject1/src/test/java/pac1/ContactTest.java
@@ -0,0 +1,15 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest() {
+		System.out.println("execute createContactTest");
+	}
+	
+	@Test
+	public void modifyContactTest() {
+		System.out.println("execute modifyContactTest");
+	}
+}
